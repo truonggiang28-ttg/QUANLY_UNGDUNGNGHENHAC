76bd03a1cc8986d4565f96e7954fe64bcb597f61
Thái Trường GIang <truonggiang121604@gmail.com>
2025-12-02 17:28:02 +0700

    sua
    Removed comments and cleaned up the interface documentation.

--- qlnhac/IKhuVuc.java
+++ qlnhac/IKhuVuc.java
@@ -1,9 +1,6 @@
 package qlnhac;
 
-/**
- * Interface định nghĩa "hợp đồng" cho các lớp có khả năng
- * phân loại theo khu vực.
- */
+//phân loại theo khu vực.
 public interface IKhuVuc {
     /**
      * Trả về tên khu vực của bài hát (ví dụ: "VPOP", "KPOP", "USUK")
